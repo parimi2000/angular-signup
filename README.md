23514f5bdb0918fe2e1347b0b5ff2a241e40ba48
parimi2000 <srisaitejaparimi@gmail.com>
2021-08-29 22:02:26 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-6g7kxs
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-6g7kxs)
---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ButtonComponent } from './button/button.component';
+
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, TopBarComponent,ProductListComponent, ButtonComponent],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
